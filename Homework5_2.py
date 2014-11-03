52089f7dae412b86427b65a35694d30c36d98942
HANA <hh@HANAs-MacBook-Pro.local>
2014-11-02 23:48:28 -0500

    program2, 1st draft

---
+++ Homework5_2.py
@@ -0,0 +1,33 @@
+from random import randrange
+
+print("\n\n__________Integer Divisions__________")
+	
+#The game about learning 'Integer Divisions' for kids
+
+try:
+	play = "yes"
+	while (play == "yes"):
+		a = randrange(5)
+		x = randrange(5)
+		answer = a // x 
+		print ("  Integer Divisions of "+ str(a)+"/" + str(x))
+		guess = input ("  ")
+		if answer == int(guess):
+			print ("  CORRECT !")
+		else:
+			print("  ERROR")
+		play = input("  Do you want to play more? ")
+	else:
+		print("  Thanks for playing")
+
+except ValueError:
+    print("  ERROR: Please enter Integers Only!")
+    pass
+
+except ZeroDivisionError:
+	print("  ERROR: progaram error, please try again")
+	pass
+
+except Exception as h:
+    print("  ERROR: Unexpected error")
+    pass
